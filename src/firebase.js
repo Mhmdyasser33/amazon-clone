32f56bf3b0e230377981baef3dec5a544c820558
Mhmdyasser33 <muhmmed.yasserxi@gmail.com>
2023-08-05 14:01:53 -0700

    hide secret information in firebase

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDY0rRY0TN7FM2VAPNobyjjkoQn2mxK-wc",
+  apiKey:process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
